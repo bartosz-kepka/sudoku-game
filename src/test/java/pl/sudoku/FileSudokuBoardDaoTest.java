a304c15e3e6ba84fa0476d0d47ef439c5d3618e9
224326 Kępka Bartosz <224326@edu.p.lodz.pl>
2020-04-22 09:04:44 +0200

    Added finalize test for FileSudokuBoardDao

--- src/test/java/pl/sudoku/FileSudokuBoardDaoTest.java
+++ src/test/java/pl/sudoku/FileSudokuBoardDaoTest.java
@@ -2,12 +2,15 @@ package pl.sudoku;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FileSudokuBoardDaoTest {
 
     @Test
-    void read_write_fromProperFile_ShouldNotThrow_Exception() {
+    public void read_write_FromProperFile_ShouldNotThrowException() {
         SudokuBoardDaoFactory factory = new SudokuBoardDaoFactory();
         SudokuBoard sudokuBoard = new SudokuBoard(new BacktrackingSudokuSolver());
         SudokuBoard sudokuBoard2;
@@ -20,18 +23,30 @@ class FileSudokuBoardDaoTest {
     }
 
     @Test
-    void read_from_nonExistentFile_ShouldThrowException() {
+    public void read_FromNonExistentFile_ShouldThrowException() {
         SudokuBoardDaoFactory factory = new SudokuBoardDaoFactory();
         Dao<SudokuBoard> fileSudokuBoardDao;
         fileSudokuBoardDao = factory.getFileDao("nonExistent");
         assertThrows(RuntimeException.class,()-> fileSudokuBoardDao.read());
     }
+
     @Test
-    void write_to_ImproperFile_ShouldThrowException() {
+    public void write_ToImproperFile_ShouldThrowException() {
         Dao<SudokuBoard> fileSudokuBoardDao;
         SudokuBoardDaoFactory factory = new SudokuBoardDaoFactory();
         SudokuBoard sudokuBoard = new SudokuBoard(new BacktrackingSudokuSolver());
         fileSudokuBoardDao = factory.getFileDao("/:;:");
         assertThrows(RuntimeException.class,()-> fileSudokuBoardDao.write(sudokuBoard));
+    }
+
+    @Test
+    public void finalize_ShouldNotThrowAnyException() throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
+        SudokuBoardDaoFactory factory = new SudokuBoardDaoFactory();
+        Dao<SudokuBoard> fileSudokuBoardDao = factory.getFileDao("Test");
+
+        Method finalize = fileSudokuBoardDao.getClass().getDeclaredMethod("finalize");
+        finalize.setAccessible(true);
+
+        assertDoesNotThrow(() -> finalize.invoke(fileSudokuBoardDao));
     }
 }
--- src/test/java/pl/sudoku/SudokuBoardDaoFactoryTest.java
+++ src/test/java/pl/sudoku/SudokuBoardDaoFactoryTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class SudokuBoardDaoFactoryTest {
 
     @Test
-    void getFileDao_ShouldNotReturn_null() {
+    public void getFileDao_ShouldNotReturnNull() {
         SudokuBoardDaoFactory factory = new SudokuBoardDaoFactory();
         assertNotNull(factory.getFileDao("testfile.txt"));
     }

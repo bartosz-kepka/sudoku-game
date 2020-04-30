50e2a61a44608db62db1745fd9274d4997102a71
aantczakpiotr <224248@edu.p.lodz.pl>
2020-04-30 22:57:03 +0200

    implemented-Listner-for-View-change

--- View/src/main/java/pl/sudoku/view/GameController.java
+++ View/src/main/java/pl/sudoku/view/GameController.java
@@ -3,6 +3,10 @@ package pl.sudoku.view;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -10,6 +14,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import pl.sudoku.model.BacktrackingSudokuSolver;
 import pl.sudoku.model.SudokuBoard;
@@ -64,9 +69,28 @@ public class GameController implements Initializable {
         for (int row = 0; row < boardSize; row++) {
             for (int column = 0; column < boardSize; column++) {
                 int fieldValue = sudokuBoard.get(row, column);
-                sudokuGrid.add(SudokuTextFieldFactory.getSudokuTextField(fieldValue), row,
-                        column);
+                TextField textField = SudokuTextFieldFactory.getSudokuTextField(fieldValue);
+                changeFiledValue(textField,row,column);
+                sudokuGrid.add(textField, row, column);
             }
         }
+    }
+
+    private void changeFiledValue(TextField textField, int row, int column) {
+        textField.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String oldVal, String newVal) {
+                if (!validateValue(newVal)) {
+                    Platform.runLater(textField::clear);
+                } else {
+                    sudokuBoard.set(row, column, Integer.parseInt(newVal));
+
+                }
+            }
+        });
+    }
+
+    private boolean validateValue(String newVal) {
+        return newVal.length() == 1 & "123456789".contains(newVal);
     }
 }

5eef1b3a0d54514b57a49ebcfc9de65498fd416b
224326 Kępka Bartosz <224326@edu.p.lodz.pl>
2020-05-19 21:56:54 +0200

    Test fix

--- JdbcSudokuBoardDao/src/test/java/pl/sudoku/jdbcsudokuboarddao/ExceptionsTest.java
+++ JdbcSudokuBoardDao/src/test/java/pl/sudoku/jdbcsudokuboarddao/ExceptionsTest.java
@@ -3,6 +3,7 @@ package pl.sudoku.jdbcsudokuboarddao;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.Locale;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,21 +11,25 @@ public class ExceptionsTest {
 
     @Test
     public void readException_ShouldReturnProper_LocalizedMessage() {
+        Locale.setDefault(new Locale("en"));
         assertEquals(new JdbcDaoReadException(new IOException()).getLocalizedMessage(), "SudokuBoard object could not be read from database");
     }
 
     @Test
     public void writeException_ShouldReturnProper_LocalizedMessage() {
+        Locale.setDefault(new Locale("en"));
         assertEquals(new JdbcDaoWriteException(new IOException()).getLocalizedMessage(), "SudokuBoard object could not be written to database");
     }
 
     @Test
     public void ConnectException_ShouldReturnProper_LocalizedMessage() {
+        Locale.setDefault(new Locale("en"));
         assertEquals(new JdbcDaoConnectException(new IOException()).getLocalizedMessage(), "Failed to connect to Sudoku database");
     }
 
     @Test
     public void DeleteException_ShouldReturnProper_LocalizedMessage() {
+        Locale.setDefault(new Locale("en"));
         assertEquals(new JdbcDaoDeleteException(new IOException()).getLocalizedMessage(), "SudokuBoard object could not be deleted from database");
     }
 }
